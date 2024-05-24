6a8ac150980eb4d08a263ecff4a7cc6ea6a0501f
Débora Andrade <deboraellenandrade@gmail.com>
2024-05-24 10:37:23 +0100

    criei atividades da segunda aula

---
+++ aulaConstEVar.py
@@ -0,0 +1,22 @@
+# Dados Primitivos:
+#Inteiro (int), real (float), boleano (bool - true/false), caractere (string - str)
+
+# Variáveis do Tipo Coleções:
+#Listas, tuplas, dicionários
+
+#Saída de dados: print("")
+#Comandos de entradas: Duas formas, como atribuição, dando uma variável para guardar
+# o dado. A outra forma é a função input()
+
+nome = "Débora"
+apelido = "Andrade"
+idade = 29
+print("O meu nome é " + nome + ", meu apelido é " + apelido + " e minha idade é " + str(idade) + ".")
+
+print("Olá, bom dia, aluna!")
+nome = input("Qual é seu nome?")
+print("Seu nome é: " + nome)
+
+
+x = int(input("Digite um valor: "))
+print(x)
---
+++ fichaAtividade01.py
@@ -0,0 +1,1 @@
+print("Olá! Minha primeira mensagem.")
