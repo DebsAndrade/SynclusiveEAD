94bfdf1dcd84f8a534e32bc26f0a11d2cd26e79d
Débora Andrade <deboraellenandrade@gmail.com>
2024-05-24 10:54:53 +0100

    atividade 01 e 02 da ficha 02

---
+++ fichaAtividade02.py
@@ -0,0 +1,16 @@
+# Exercício 01
+
+nome = input("Olá, digite seu nome: ")
+print("Olá,",nome)
+
+# Exercício 02
+
+variavel_inteira = 100
+variavel_float = 100.02
+variavel_boolean = True
+variavel_string = "true"
+
+print(type(variavel_boolean))
+print(type(variavel_float))
+print(type(variavel_inteira))
+print(type(variavel_string))
