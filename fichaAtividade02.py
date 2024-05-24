85b4e3ec8c079ae3cb319e90458dbc729f715d71
Débora Andrade <deboraellenandrade@gmail.com>
2024-05-24 11:08:23 +0100

    atividade 04

--- fichaAtividade02.py
+++ fichaAtividade02.py
@@ -23,3 +23,19 @@ n = 200
 
 resultado = pi * n
 print("O valor da multiplicação de ", pi, "por", n, "é", resultado)
+
+# Exercício 04
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+print("A soma é: ", numero1+numero2)
+subtracao = numero1 - numero2
+print("A subtração é: ", subtracao)
+print("A multiplicação é: ", numero1*numero2)
+print("A divisão é: ", numero1/numero2)
+
+print(numero1+numero2)
+print(numero1-numero2)
+print(numero1*numero2)
+print(numero1/numero2)
