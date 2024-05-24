7b009132c31867041c1298249bb592bf70574ab4
Débora Andrade <deboraellenandrade@gmail.com>
2024-05-24 11:02:15 +0100

    atividade 03

--- fichaAtividade02.py
+++ fichaAtividade02.py
@@ -14,3 +14,12 @@ print(type(variavel_boolean))
 print(type(variavel_float))
 print(type(variavel_inteira))
 print(type(variavel_string))
+
+
+# Exercício 03
+
+pi = 3.1415
+n = 200
+
+resultado = pi * n
+print("O valor da multiplicação de ", pi, "por", n, "é", resultado)
