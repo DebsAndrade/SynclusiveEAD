67cc5d7fb06877cf0df72527479fa0d9d31e47e2
Débora Andrade <deboraellenandrade@gmail.com>
2024-05-28 15:05:35 +0100

    atividade ficha 3 exercicio 1 opção 3

--- unidade_02/fichaAtividade03.py
+++ unidade_02/fichaAtividade03.py
@@ -3,7 +3,7 @@ idade = int(input("Me diga sua idade hoje: "))
 if idade > 18:
     print("Ok! Pode conduzir.")
 else:
-    print("OPS! Você AINDA NÃO pode conduzir.")
+    print("OPS! Você AINDA NÃO pode conduzir. Espere mais", (18 - idade), "anos!")
 
 
 #if idade < 18:
